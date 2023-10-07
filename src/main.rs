0554fd3526271f56da054ff0b9a303462e9db3e7
Andrew <andrew@wsa.kz>
2023-10-07 19:02:44 +0600

    init commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{HttpServer, App, web::Data};
+use sqlx::{Pool, mysql::MySqlPoolOptions, MySql};
+use dotenv::dotenv;
+use std::io::Result;
+
+pub struct AppState {
+    db: Pool<MySql>
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    let connecrion_string = std::env::var("DATABASE_URL").expect("Строка подлючения должна быть установлена!");
+    let pool: Pool<MySql> = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&connecrion_string)
+        .await.expect("Ошибка пула подключений!");
+    sqlx::migrate!("./migrations").run(&pool).await.expect("Произошла ошибка миграции!");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(AppState{db: pool.clone()}))
+    }).bind(("127.0.0.1", 8080))?.run().await
+}
