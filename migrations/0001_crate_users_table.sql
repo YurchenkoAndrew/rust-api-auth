4029821fa5bfeb27efd85261279d90388d7c2a43
YurchenkoAndrew <webmaster@wsa.kz>
2023-10-27 04:53:21 +0600

    Changed tables in migrations

--- migrations/0001_crate_users_table.sql
+++ migrations/0001_crate_users_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     email VARCHAR(50) NOT NULL UNIQUE,
     email_verified_at TIMESTAMPTZ,
     password VARCHAR(255) NOT NULL,
-    remember_token VARCHAR(100),
+    remember_token VARCHAR(255),
     created_at TIMESTAMPTZ DEFAULT current_timestamp NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT current_timestamp NOT NULL,
     active BOOLEAN NOT NULL
--- migrations/0002_create_access_tokens_table.sql
+++ migrations/0002_create_access_tokens_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE access_tokens (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     expires_at TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMPTZ DEFAULT current_timestamp NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT current_timestamp NOT NULL
